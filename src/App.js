44481c486e4073d69b54b93695d291ed38861d91
KradasA4 <wichai.sw19@gmail.com>
2020-05-23 23:07:11 +0700

    add hooks with object

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const expensiveInitialState = () => {
-  return 10
-}
 
 function App() {
-  const [ count, setCount] = useState(() => expensiveInitialState());
+  const [ {count, count2}, setCount] = useState({count: 10, count2: 20});
 
   return (
     <div>
-      {count}
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
       <br/>
       {/* using Updater Function with Hook 
-      Using an updater function will preserve the order of how state should be applied as well as make sure all previous states are flushed through.
+      * Using an updater function will preserve the order of how state should be applied as well as make sure all previous states are flushed through.
+      * Avoid race condition which get the update overwritten.
       */}
-      <button onClick={() => setCount(currentCount => currentCount + 1)}>Increase</button>
+      <button 
+        onClick={() =>
+          setCount(currentState => ({
+            ...currentState,
+            count: currentState.count +1 
+          }) 
+        )}
+      >
+        Increase
+      </button>
     </div>
   );
 }
